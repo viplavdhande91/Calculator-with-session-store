6e55f11ade924510a2afcbca0f7ecd31c25b3245
Ruvinda Dhambarage <ruvinda.dhambarage@nagarro.com>
2022-03-30 20:36:17 +0530

    fix MyException

--- MyException.h
+++ MyException.h
@@ -1,16 +1,13 @@
 #ifndef MYEXCEPTION_H
 #define MYEXCEPTION_H
 
-#include <iostream>
-using namespace std;
-
 // CUSTOM EXCEPTION CLASS
-class MyException : public exception
+class MyException : public std::exception
 {
 public:
     const char* what() const throw()
     {
-        return "Exception Occured: Number is out of range\n";
+        return "Exception Occurred: Number is out of range\n";
     }
 };
 
